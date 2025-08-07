6b843a8bde8ce05fa83a876c03704b3837ea2643
ajcur <173679835+ajcur@users.noreply.github.com>
2025-08-07 18:01:35 +0200

    Add buttons and refactor gameplay to be triggered by button presses

--- javascript.js
+++ javascript.js
@@ -1,18 +1,40 @@
+let roundNumber = 0;
+let computerScore = 0;
+let humanScore = 0;
+
 function getComputerChoice() {
     let options = ['Rock', 'Paper', 'Scissors'];
     let index = Math.floor(Math.random() * options.length);
     let computerChoice = options[index];
     return computerChoice;
-}
+};
 
-function getHumanChoice() {
+let buttons = document.querySelector("#buttons");
+
+buttons.addEventListener("click", function(e) {
+    roundNumber++;
+    let humanChoice = "";
+    switch (e.target.id) {
+        case "btn-rock":
+            humanChoice = "Rock";
+            break;
+        case "btn-paper":
+            humanChoice = "Paper";
+            break;
+        case "btn-scissors":
+            humanChoice = "Scissors";
+    }
+    playRound(humanChoice);
+});
+
+/* function getHumanChoice() {
     let humanChoice = prompt("Enter your choice: ");
     humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1).toLowerCase();
     if (humanChoice !== 'Rock' && humanChoice !== 'Scissors' && humanChoice !== 'Paper') {
         humanChoice = undefined;
     }
     return humanChoice;
-}
+} */
 
 function determineRoundWinner(computerChoice, humanChoice) {
     let roundWinner;
@@ -22,38 +44,44 @@ function determineRoundWinner(computerChoice, humanChoice) {
                 roundWinner = 'tie';
             } else if (computerChoice == 'Paper') {
                 roundWinner = 'computer';
+                computerScore++;
             } else if (computerChoice == 'Scissors') {
                 roundWinner = 'human';
+                humanScore++;
             }
             break;
         case 'Paper':
             if (computerChoice == 'Rock') {
                 roundWinner = 'human';
+                humanScore++;
             } else if (computerChoice == 'Paper') {
                 roundWinner = 'tie';
             } else if (computerChoice == 'Scissors') {
                 roundWinner = 'computer';
+                computerScore++;
             }
             break; 
         case 'Scissors':
             if (computerChoice == 'Rock') {
                 roundWinner = 'computer';
+                computerScore++;
             } else if (computerChoice == 'Paper') {
                 roundWinner = 'human';
+                humanScore++;
             } else if (computerChoice == 'Scissors') {
                 roundWinner = 'tie';
             }
             break;
     }
     return roundWinner;
-}
+};
 
-function playRound(roundNumber) {
-    let humanChoice = getHumanChoice();
+function playRound(humanChoice) {
+/*     let humanChoice = getHumanChoice();
     if (!humanChoice) {
         console.log('You might have had a spelling error – try again!');
         return;
-    }
+    } */
     let computerChoice = getComputerChoice();
     console.log(`Computer chose: ${computerChoice}!`)
     let roundWinner = determineRoundWinner(computerChoice, humanChoice);
@@ -67,10 +95,17 @@ function playRound(roundNumber) {
         case 'tie':
             console.log(`Round ${roundNumber} is a tie! Try again!`);
     }
+    console.log(`Human Score: ${humanScore}`);
+    console.log(`Computer Score: ${computerScore}`);
+    if (humanScore >= 5) {
+        console.log("Game Over! Human wins the game!");
+    } else if (computerScore >= 5) {
+        console.log("Game Over! Computer wins the game!");
+    }
     return roundWinner;
-}
+};
 
-function playGame() {
+/* function playGame() {
     let roundNumber = 1;
     let humanScore = 0;
     let computerScore = 0;
@@ -94,4 +129,4 @@ function playGame() {
     } else console.log('You win the game!');
 }
 
-playGame();
+playGame(); */
